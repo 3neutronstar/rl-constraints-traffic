e60fa72fc936bf38c46605607069c01b2c339e12
3neutronstar <minsoo.kang0918@gmail.com>
2021-02-22 09:02:58 +0900

    del dup in test and sim

--- test.py
+++ test.py
@@ -110,22 +110,25 @@ def city_dqn_test(flags, sumoCmd, configs):
             mask_matrix[~clear_matrix] = False
             # check performance
             for _, interests in enumerate(configs['interest_list']):
+                # delete 중복
+                dup_list=list()
                 for interest in interests:
                     # 신호군 흐름
-                    if interest['inflow'] != None:
-                        # 차량의 대기시간
-                        # 차량이 있을 때만
+                    if interest['inflow'] != None and interest['inflow'] not in dup_list:
+                        # 차량의 대기시간, 차량이 있을 때만
                         if traci.edge.getLastStepVehicleNumber(interest['inflow']) != 0:
                             avg_waiting_time += traci.edge.getWaitingTime(interest['inflow'])/float(
                                 traci.edge.getLastStepVehicleNumber(interest['inflow']))
                             # 차량의 평균속도
                             part_velocity.append(
                                 traci.edge.getLastStepMeanSpeed(interest['inflow']))
+                        dup_list.append(interest['inflow'])
 
-                    if interest['outflow'] != None:
+                    if interest['outflow'] != None and interest['inflow'] not in dup_list:
                         if traci.edge.getLastStepVehicleNumber(interest['outflow']) != 0:
                             part_velocity.append(
                                 traci.edge.getLastStepMeanSpeed(interest['outflow']))
+                        dup_list.append(interest['outflow'])
 
             # # 전체 흐름
             # vehicle_list = traci.vehicle.getIDList()
